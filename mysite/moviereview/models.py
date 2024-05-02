3679c13a718bbc70add733c7d4e9479469d23955
Sayed Husain Alawi <shalawi@mail.fhsu.edu>
2024-05-02 11:32:18 +0300

    Added Movie(models.Model) into models.py, ran makemigrations, and ran migrate
    added the APIs in tests.py

--- mysite/moviereview/models.py
+++ mysite/moviereview/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+
+class Movie(models.Model):
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    release_date = models.DateField()
+    imdb_id = models.CharField(max_length=20, unique=True)
+    imdb_rating = models.FloatField()
+    runtime = models.IntegerField()
+    genres = models.CharField(max_length=255)  # Simplified for example
+    stars = models.TextField()  # Text field to store list of names
+    directors = models.TextField()  # Text field to store list of names
+    poster = models.ImageField(upload_to='posters/')
+
+    def __str__(self):
+        return self.title
--- mysite/moviereview/tests.py
+++ mysite/moviereview/tests.py
@@ -1,3 +1,37 @@
-from django.test import TestCase
 
-# Create your tests here.
+
+# Download an image
+import requests
+
+url = "https://movies-tv-shows-database.p.rapidapi.com/"
+
+querystring = {"movieid":"tt0103064"}
+
+headers = {
+	"Type": "get-movies-images-by-imdb",
+	"X-RapidAPI-Key": "192b8070d4mshdce2e96668d0f65p180a1ejsn041cca2faf18",
+	"X-RapidAPI-Host": "movies-tv-shows-database.p.rapidapi.com"
+}
+
+response = requests.get(url, headers=headers, params=querystring)
+
+print(response.json())
+
+
+#get movie info
+
+# import requests
+#
+# url = "https://movies-tv-shows-database.p.rapidapi.com/"
+#
+# querystring = {"movieid":"tt0103064"}
+#
+# headers = {
+# 	"Type": "get-movie-details",
+# 	"X-RapidAPI-Key": "192b8070d4mshdce2e96668d0f65p180a1ejsn041cca2faf18",
+# 	"X-RapidAPI-Host": "movies-tv-shows-database.p.rapidapi.com"
+# }
+#
+# response = requests.get(url, headers=headers, params=querystring)
+#
+# print(response.json())
