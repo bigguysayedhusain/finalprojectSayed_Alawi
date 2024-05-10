f55841dfda1c6579f89a6c5544cda139143a7a7b
Sayed Husain Alawi <shalawi@mail.fhsu.edu>
2024-05-10 13:44:27 +0300

    Updated the Admin panel

--- mysite/moviereview/admin.py
+++ mysite/moviereview/admin.py
@@ -1,6 +1,46 @@
 from django.contrib import admin
+from .models import Movie, Review
 
-from .models import Movie,Review
 
-admin.site.register(Movie)
-admin.site.register(Review)
+# Inline admin for Review model to edit reviews directly from the Movie admin page
+class ReviewInline(admin.TabularInline):
+    model = Review
+    extra = 1  # Number of extra blank forms to show
+    fields = ['user', 'text', 'rating', 'created_at']
+    readonly_fields = ['created_at']  # Make created_at field read-only
+
+
+# Custom action to reset IMDb ratings
+def reset_imdb_ratings(modeladmin, request, queryset):
+    queryset.update(imdb_rating=0)
+
+
+reset_imdb_ratings.short_description = "Reset IMDb ratings for selected movies"
+
+
+# Admin configuration for Movie model
+class MovieAdmin(admin.ModelAdmin):
+    list_display = ('title', 'release_date', 'imdb_rating', 'number_of_reviews')
+    search_fields = ['title', 'imdb_id']
+    list_filter = ['release_date', 'imdb_rating']
+    inlines = [ReviewInline]
+    actions = [reset_imdb_ratings]
+
+    # Helper method to display the number of reviews directly in the admin list view
+    def number_of_reviews(self, obj):
+        return obj.reviews.count()
+
+    number_of_reviews.short_description = 'Number of Reviews'
+
+
+# Admin configuration for Review model
+class ReviewAdmin(admin.ModelAdmin):
+    list_display = ('movie', 'user', 'rating', 'created_at')
+    list_select_related = ['movie', 'user']  # Optimizes query performance
+    search_fields = ['movie__title', 'user__username']
+    list_filter = ['rating', 'created_at']
+
+
+# Registering the models with their respective admin configuration
+admin.site.register(Movie, MovieAdmin)
+admin.site.register(Review, ReviewAdmin)
--- mysite/moviereview/tests.py
+++ mysite/moviereview/tests.py
@@ -1,11 +1,3 @@
-# TODO Create Home page
-
 # TODO adjust the links to be left and right
 
-# TODO fix the add review. it should be a button (Add Review). Also, the same user can not create more than 1 review
-
-# TODO add date published to reviews
-
 # TODO make sure the README is correct
-
-# TODO fix permission of being able to add, change, and delete
